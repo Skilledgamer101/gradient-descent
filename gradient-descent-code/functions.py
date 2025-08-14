1a2976c987ff80533346e53b8f19b2079d7b386a
Mansoor Lunawadi <136755975+Skilledgamer101@users.noreply.github.com>
2025-08-14 16:51:44 -0400

    Add files via upload

--- gradient-descent-code/functions.py
+++ gradient-descent-code/functions.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import itertools
 
 # Data
 data = [
@@ -12,11 +14,13 @@ data = [
         [2.37, 24]
         ]
 N = len(data)
+iteration = 0
 
 # Functions to perform gradient descent
 
 # Plot data points and regression line
 def plot(w, b):
+    
     # Separate into x and y lists
     x_data = [point[0] for point in data]
     y_data = [point[1] for point in data]
@@ -38,6 +42,15 @@ def plot(w, b):
     plt.grid(True)
     plt.show()
 
+# Plot MSE Losses vs Iterations
+def plot_MSE(MSE_losses):
+
+    plt.plot(MSE_losses, marker='o')
+    plt.xlabel("Iteration")
+    plt.ylabel("Loss")
+    plt.title("MSE Loss vs Iterations")
+    plt.grid(True)
+    plt.show()
 
 # Perform full batch gradient descent
 def full_batch(w, b, learning_rate):
@@ -46,17 +59,83 @@ def full_batch(w, b, learning_rate):
     # Weight derivative = 2/N * (SUM(predicted y value - actual y value) * x value)
     # Bias derivative = 2/N * (SUM(predicted y value - actual y value))
     # MSE Loss = 1/N * SUM((actual - predicted) ** 2)
-    weight_derivative, bias_derivative = 0, 0
+
+    weight_derivative, bias_derivative, MSE_loss = 0, 0, 0
     for actual_x, actual_y in data:
         predicted_y = w * actual_x + b
         weight_derivative += ((predicted_y - actual_y) * actual_x)
         bias_derivative += (predicted_y - actual_y)
+        MSE_loss += ((actual_y - predicted_y) ** 2)
 
     weight_derivative *= (2/N)
     bias_derivative *= (2/N)
+    MSE_loss *= (1/N)
 
     w -= (weight_derivative * learning_rate)
     b -= (bias_derivative * learning_rate)
+    global iteration
+    iteration += 1
 
-    print(f"w = {w}, b = {b}")
-    return w, b
+    print(f"w = {w}, b = {b}, MSE Loss = {MSE_loss}, Iteration = {iteration}")
+    return w, b, MSE_loss
+
+# Perform stochiatic gradient descent
+def stochiatic(w, b, learning_rate):
+    
+    # Formulas
+    # Weight derivative = 2 * ((predicted y value - actual y value) * x value)
+    # Bias derivative = 2 * ((predicted y value - actual y value))
+    # MSE Loss = ((actual - predicted) ** 2)
+
+    weight_derivative, bias_derivative, MSE_loss = 0, 0, 0
+
+    # Pick random x,y pair from data
+    random_idx = random.randint(0, len(data) - 1)
+    actual_x, actual_y = data[random_idx]
+    predicted_y = w * actual_x + b
+    weight_derivative += (2 * (predicted_y - actual_y) * actual_x)
+    bias_derivative += (2 * (predicted_y - actual_y))
+    MSE_loss += ((actual_y - predicted_y) ** 2)
+
+
+
+    w -= (weight_derivative * learning_rate)
+    b -= (bias_derivative * learning_rate)
+    global iteration
+    iteration += 1
+
+    print(f"w = {w}, b = {b}, MSE Loss = {MSE_loss}, Iteration = {iteration}")
+    return w, b, MSE_loss
+
+
+# Perform mini batch stochiatic gradient descent
+def mini_batch(w, b, learning_rate, batch_size):
+    
+    # Formulas (M = batch_size)
+    # Weight derivative = 2/M * (SUM(predicted y value - actual y value) * x value)
+    # Bias derivative = 2/M * (SUM(predicted y value - actual y value))
+    # MSE Loss = 1/M * SUM((actual - predicted) ** 2)
+
+    weight_derivative, bias_derivative, MSE_loss = 0, 0, 0
+
+    # Pick random batch of x,y pair from data
+    random_idx_start = random.randint(0, len(data) - 1)
+    random_idx_end = min(len(data), (random_idx_start + batch_size)) # end is NON INCLUSIVE
+
+    for actual_x, actual_y in data[random_idx_start: random_idx_end]:
+        predicted_y = w * actual_x + b
+        weight_derivative += ((predicted_y - actual_y) * actual_x)
+        bias_derivative += (predicted_y - actual_y)
+        MSE_loss += ((actual_y - predicted_y) ** 2)
+
+    weight_derivative *= (2/batch_size)
+    bias_derivative *= (2/batch_size)
+    MSE_loss *= (1/batch_size)
+
+    w -= (weight_derivative * learning_rate)
+    b -= (bias_derivative * learning_rate)
+    global iteration
+    iteration += 1
+
+    print(f"w = {w}, b = {b}, MSE Loss = {MSE_loss}, Iteration = {iteration}")
+    return w, b, MSE_loss
--- gradient-descent-code/main.py
+++ gradient-descent-code/main.py
@@ -1,15 +1,34 @@
 import functions as fn
-from time import sleep
         
 def main():
-        w, b = 0, 0
-        for _ in range(1000):
-                w, b = fn.full_batch(w, b, learning_rate=0.01)
-                
-        fn.plot(w, b)
-      #  fn.stochastic()
-      #  fn.mini_batch()
+    w, b, MSE_loss_old = 0, 0, 0
+    MSE_losses = []
+    streak = 1
+    
+    while True:
+        w, b, MSE_loss_new = fn.full_batch(w, b, learning_rate=0.01)        # Replace fn.full_batch with fn.stochastic() or fn.mini_batch() -- mini batch has batch_size parameter too! batch size is between 1 and len(data) = 7, inclusive
+
+        if MSE_loss_new == MSE_loss_old:
+            streak += 1
+
+        else:
+            streak = 1
+
+        MSE_loss_old = MSE_loss_new
+        MSE_losses.append(MSE_loss_old)
+
+        
+        # Stop after last 5 MSE Loss values are the same OR # iterations >= 100,000 
+        # Latter needed for stochiatic SGD where last 5 MSE Loss are usually never the same
+        if streak == 5 or len(MSE_losses) >= 100000:
+            break
+        
+    # Plot the data points and the final regression line
+    fn.plot(w, b)
+
+    # Plot the MSE loss curve
+    fn.plot_MSE(MSE_losses)
 
 if __name__ == "__main__":
-        main()
+    main()
 
